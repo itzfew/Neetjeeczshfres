bc72416e3a4855badd9a71e8013c2ab4b93e9641
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-06-12 01:04:34 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
+import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
-
-export default MyApp;
