13ab78875802bc0b55ecdcae30ed46fa30296bb6
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-06-12 07:22:05 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
-import { AuthProvider } from '../context/AuthContext';
-import '../styles/globals.css';
+import { AuthProvider } from '../contexts/AuthContext';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import '../styles/globals.css';
+import Head from 'next/head';
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <script src="https://sdk.cashfree.com/js/v3/cashfree.js" async></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js" async></script>
+      </Head>
       <Component {...pageProps} />
+      <ToastContainer />
     </AuthProvider>
   );
 }
