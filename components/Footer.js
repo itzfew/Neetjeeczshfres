9be500475ca6dd4360fb0f14e10bbeb8dafb94d7
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-06-12 08:09:00 +0530

    Delete components directory

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2025 CourseApp. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
